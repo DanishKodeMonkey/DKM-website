9dd1af7e481492c4c7dfd03ddadc8f8b2fa90092
danishkodemonkey <humberjini@hotmail.com>
2024-10-10 18:06:05 +0200

    test api

--- api/main.ts
+++ api/main.ts
@@ -1,29 +1,28 @@
-import {Application, Router} from "@oak/oak";
-import {oakCors} from "@tajpouria/cors"
-import data from "./data.json" with {type:"json"}
+import { Application, Router } from "@oak/oak";
+import { oakCors } from "@tajpouria/cors";
+import data from "./data.json" with { type: "json" };
 
-
-const router = new Router()
+const router = new Router();
 
 router.get("/api/dinosaurs", (context) => {
-    context.response.body = data;
-  });
+  context.response.body = data;
+});
 
-router.get("/api/dinosaurs/:dinosaur", (context)=>{
-    if(!context?.params?.dinosaur){
-        context.response.body = "No dinosaur name provided."
-    }
+router.get("/api/dinosaurs/:dinosaur", (context) => {
+  if (!context?.params?.dinosaur) {
+    context.response.body = "No dinosaur name provided.";
+  }
 
-    const dinosaur = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
+  const dinosaur = data.find((item) =>
+    item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
+  );
 
-    context.response.body = dinosaur ?? "No dinosaur found"
-})
+  context.response.body = dinosaur ?? "No dinosaur found";
+});
 
-const app = new Application()
-app.use(oakCors())
-app.use(router.routes())
-app.use(router.allowedMethods())
+const app = new Application();
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-await app.listen({port:8000})
+await app.listen({ port: 8000 });
