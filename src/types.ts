0fe679771e2af4850725362867b38d8bd8519fcc
danishkodemonkey <humberjini@hotmail.com>
2024-10-11 17:58:33 +0200

    add types for QRcode interface

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,8 @@
 export type Dino = { name: string; description: string };
+
+export interface QRCodeProps {
+    value: string; // URL to encode in QR
+    size?: number; // optional size for QR
+    bgColor?: string; //optional bg color for QR
+    fgColor?: string; //optional fg color for QR
+}
