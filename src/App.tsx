87bfed687dd03e81daac552aafb26f301cdfc317
danishkodemonkey <humberjini@hotmail.com>
2024-10-11 17:58:21 +0200

    reroute index / to businessCard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/index.tsx";
 import Dinosaur from "./pages/dinosaur.tsx";
 import "./App.css";
+import BusinessCardPage from "./pages/businessCard.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<BusinessCardPage />} />
         <Route path="/:selectedDinosaur" element={<Dinosaur />} />
       </Routes>
     </BrowserRouter>
