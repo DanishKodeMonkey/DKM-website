edb74c2c5f0243f896f6b7994e39fa917f38154b
danishkodemonkey <humberjini@hotmail.com>
2024-10-18 15:24:35 +0200

    Clean up for main site

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import "./App.css";
 import BusinessCardPage from "./pages/businessCard.tsx";
 
 function App() {
-  console.log("APP component rendered");
-
   return (
     <HashRouter>
       <Routes>
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface QRCodeProps {
-    value: string; // URL to encode in QR
-    size?: number; // optional size for QR
-    bgColor?: string; //optional bg color for QR
-    fgColor?: string; //optional fg color for QR
-}
