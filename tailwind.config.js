7e08e7dcdcb8b7e084b0c15eb1361ff2353a9835
danishkodemonkey <humberjini@hotmail.com>
2024-10-22 16:23:49 +0200

    Install tailwindcss and configure

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js, ts, jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:{ /* Represents main colors for interactive elements: Blue pallette */
+          DEFAULT: '#2563eb',
+          light: '#60a5fa',
+          dark: '#1e3a8a',
+        },
+        secondary:{ /* represents highlights, call to actions or warnings: Amber pallette */
+          DEFAULT: '#d97706',
+          light: '#fbbf24',
+          dark: '#78350f'
+        },
+        neutral:{ /* represents backgrounds-borders and text: Slate pallette */
+          DEFAULT:'#475569',
+          light: '#94a3b8',
+          dark:'#0f172a'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
