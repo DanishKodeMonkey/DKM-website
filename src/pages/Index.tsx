c196b748f429a17ef1a5765cb7ce365eadf9fb8c
danishkodemonkey <humberjini@hotmail.com>
2024-10-22 16:45:57 +0200

    Remove inline styling in favor of globals

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,45 +16,47 @@ export default function Index() {
   }, []);
 
   return (
-    <main className="flex flex-col">
-      <div>
-        <h1 className="text-5xl">Hello there!</h1>
-        <h3 className="text-3xl">
+    <main className="container">
+      <div className="text-center responsive-text">
+        <h1>Hello there!</h1>
+        <h3>
           My name is <span className="text-sky-400">Daniel</span>
         </h3>
-        <h4 className="text-2xl">
+        <h4>
           I am a{" "}
           <span className="text-amber-400">Full-stack web developer!</span>
         </h4>
-        <p className="text-gray-400">
+        <p>
           I love creating{" "}
           <span className="font-bold text-slate-100">interesting</span> and{" "}
           <span className="font-bold text-slate-100">intuitive</span>{" "}
-          web solutions, cracking
-          <span className="font-bold text-red-500">challenges</span> and{" "}
+          web solutions, cracking 
+          <span className="font-bold text-red-500"> challenges</span> and{" "}
           <span className=" font-bold text-purple-500">learning</span>{" "}
           new things!
         </p>
-        <button>Test button</button>
-      </div>
 
-      <div className="my-5 p-2 border border-solid border-slate-400 rounded-lg w-1/2 mx-auto">
-        <h2 className=" text-lg font-extrabold text-center">
+      </div>
+      <button>Test button</button>
+      <div className="container">
+        <h2>
           Have a look at what people think of this site!
         </h2>
-        <p className="text-slate-400 text-sm text-center">
+        <p>
           Click the comments to see some details!
         </p>
         <div className="flex flex-col gap-5 my-4">
           {comments.map((comment: Comment) => {
             return (
-              <Link
-                to={`/${comment.id}`}
-                key={comment.id}
-                className="comment"
-              >
-                {comment.name}: {comment.message}
-              </Link>
+              <div className="card">
+                  <Link
+                    to={`/${comment.id}`}
+                    key={comment.id}
+                    className="comment"
+                  >
+                    {comment.name}: {comment.message}
+                  </Link>
+              </div>
             );
           })}
         </div>
