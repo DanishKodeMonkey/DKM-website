ac174cf11b19f96e62f83710c204027b5ec6c6db
danishkodemonkey <humberjini@hotmail.com>
2024-10-23 16:20:12 +0200

    include assets images

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.gif'],
+  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.gif"],
   server: {
     proxy: {
       "/api": {
