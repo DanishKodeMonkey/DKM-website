1dbeb3d22a766bc39906c76cde6bc649e5c81275
danishkodemonkey <humberjini@hotmail.com>
2024-10-24 15:17:58 +0200

    Import of various tech svg icons, attempt to make deno play nice with them (temp fix)

--- src/components/TechScroller.tsx
+++ src/components/TechScroller.tsx
@@ -1,22 +1,48 @@
-//@ts-ignore: deno check hates images
+/// <reference path="../images.d.ts" />
+/// <reference path="../vite-env.d.ts" />
+// @ts-self-types="../images.d.ts"
+
+// @ts-ignore: Deno hates image imports for now
 import bootstrapIcon from '../public/assets/bootstrap.svg';
+// @ts-ignore: Deno hates image imports for now
 import css3Icon from '../public/assets/css3.svg';
+// @ts-ignore: Deno hates image imports for now
 import denoIcon from '../public/assets/deno.svg';
+// @ts-ignore: Deno hates image imports for now
 import dockerIcon from '../public/assets/docker.svg';
+// @ts-ignore: Deno hates image imports for now
 import expressIcon from '../public/assets/express.svg';
+// @ts-ignore: Deno hates image imports for now
 import flaskIcon from '../public/assets/flask.svg';
+// @ts-ignore: Deno hates image imports for now
 import gitIcon from '../public/assets/git.svg';
+// @ts-ignore: Deno hates image imports for now
 import html5Icon from '../public/assets/html5.svg';
+// @ts-ignore: Deno hates image imports for now
 import htmxIcon from '../public/assets/htmx.svg';
+// @ts-ignore: Deno hates image imports for now
 import javascriptIcon from '../public/assets/javascript.svg';
+// @ts-ignore: Deno hates image imports for now
 import mongodbIcon from '../public/assets/mongodb.svg';
+// @ts-ignore: Deno hates image imports for now
 import mongooseIcon from '../public/assets/mongoose.svg';
+// @ts-ignore: Deno hates image imports for now
 import nodedotjsIcon from '../public/assets/nodedotjs.svg';
+// @ts-ignore: Deno hates image imports for now
 import postgresqlIcon from '../public/assets/postgresql.svg';
+// @ts-ignore: Deno hates image imports for now
 import prismaIcon from '../public/assets/prisma.svg';
+// @ts-ignore: Deno hates image imports for now
 import pythonIcon from '../public/assets/python.svg';
+// @ts-ignore: Deno hates image imports for now
 import reactIcon from '../public/assets/react.svg';
+// @ts-ignore: Deno hates image imports for now
 import tailwindcssIcon from '../public/assets/tailwindcss.svg';
+// @ts-ignore: Deno hates image imports for now
 import typescriptIcon from '../public/assets/typescript.svg';
+// @ts-ignore: Deno hates image imports for now
 import viteIcon from '../public/assets/vite.svg';
+// @ts-ignore: Deno hates image imports for now
 import webpackIcon from '../public/assets/webpack.svg';
+
+
