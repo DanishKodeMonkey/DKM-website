07cf10524727101e9b26048db9009db008cd2fda
danishkodemonkey <humberjini@hotmail.com>
2024-10-25 15:42:36 +0200

    Cleanup and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import deno from '@deno/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), deno()],
   assetsInclude: ["**/*.jpg", "**/*.png", "**/*.gif"],
   server: {
     proxy: {
