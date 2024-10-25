14ba9cb53c02271cbfd7afc8f12ab3cd76586be5
danishkodemonkey <humberjini@hotmail.com>
2024-10-25 16:12:55 +0200

    Fix commonjs oopsie to es6 imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+export default {
+  plugins: [tailwindcss, autoprefixer],
 };
