a42fc6821c8471732f5ef40342429de47a8ef587
danishkodemonkey <humberjini@hotmail.com>
2024-10-28 16:20:22 +0100

    Implement github API interactions to project api

--- api/main.ts
+++ api/main.ts
@@ -1,10 +1,18 @@
 import { Application, Router } from "@oak/oak";
 import { oakCors } from "@tajpouria/cors";
+import "jsr:@std/dotenv/load"
+import {Octokit} from '@octokit/rest'
 import data from "./data.json" with { type: "json" };
 
 /* Router  */
 const router = new Router();
+const GITHUB_TOKEN = Deno.env.get("GITHUB_TOKEN")
 
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN
+})
+
+/* Comments */
 router.get("/api/comments", (context) => {
   context.response.body = data;
 });
@@ -17,6 +25,27 @@ router.get("/api/comments/:commentId", (context) => {
 
   context.response.body = comment ?? "no comment found";
 });
+
+/* Github (portfolio) */
+router.get("/api/repos", async(context) =>{
+  try{
+    // fetch user repos
+    const response = await octokit.repos.listForAuthenticatedUser({per_page: 10,})
+    context.response.body = response.data.map((repo) =>({
+      id: repo.id,
+      name: repo.name,
+      html_url: repo.html_url,
+      description: repo.description,
+      created_at: repo.created_at, // parse to Date
+      updated_at: repo.updated_at
+      
+    }))
+  }catch(error){
+    console.error("Error fetching GitHub repos: ", error)
+    context.response.status = 500
+    context.response.body = { error : "Failed to fetch repos"}
+  }
+})
 
 /* App */
 const app = new Application();
