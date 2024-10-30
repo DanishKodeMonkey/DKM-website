2426ee10090090f107eb2158e08870bdc4116900
danishkodemonkey <humberjini@hotmail.com>
2024-10-30 16:04:10 +0100

    Various styling improvements including fonts, color scheme usage and gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,45 +4,53 @@ export default {
   darkMode: 'media',
   theme: {
     extend: {
+      fontFamily:{
+        header: ['CyberAlert', 'sans-serif'],
+      },
       colors: {
         primary: {
           /* Represents main colors for interactive elements: Blue pallette */
-          DEFAULT: "#2563eb",
-          light: "#60a5fa",
-          dark: "#1e3a8a",
+          DEFAULT: "#0f6292",
+          light: "#3f81a7",
+          dark: "#093a57",
         },
         secondary: {
           /* represents highlights, call to actions or warnings: Amber pallette */
-          DEFAULT: "#16a34a",
-          light: "#4ade80",
-          dark: "#14532d",
+          DEFAULT: "#16ff00",
+          light: "#4f3",
+          dark: "#0d9900",
         },
         tertiary: {
           /* For tertiary elements, cards, backgrounds on backgrounds, etc. */
-          DEFAULT: "#4338ca",
-          light: "#a5b4fc",
-          dark: "#312e81"
+          DEFAULT: "#ffed00",
+          light: "#fff24c",
+          dark: "#998e00"
+        },
+        highlight: {
+          DEFAULT: "#37e2d5",
+          light: "#73eae1",
+          dark: "#21877f"
         },
         neutral: {
           /* represents backgrounds-borders and text: Slate pallette */
-          DEFAULT: "#475569",
-          light: "#94a3b8",
-          dark: "#0f172a",
+          DEFAULT: "#191919",
+          light: "#cecece",
+          dark: "#000",
         },
       },
       keyframes: {
         "slide-left": {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-105%)" },
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
         },
         "slide-right": {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
         },
       },
       animation: {
-        "slide-left": "slide-left 10s linear infinite",
-        "slide-right": "slide-right 40s reverse linear infinite",
+        "slide-left": "slide-left 20s linear infinite",
+        "slide-right": "slide-right 20s reverse linear infinite",
       },
     },
   },
