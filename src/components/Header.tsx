74916df7da1a59aab6b7bc59855dfdbfb35c903c
danishkodemonkey <humberjini@hotmail.com>
2024-10-30 16:05:10 +0100

    remove image from header, add selection highlight to navbar, various styling fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,24 @@
 import _React, { useState } from "react";
 // @ts-ignore: Deno vite react hates image imports
-import DKM from "../public/assets/mm12sSNz.jpg";
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [selectedLink, setSelectedLink] = useState<string | null>(null);
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
 
+  const handleLinkClick = (link: string) => {
+    setSelectedLink(link);
+  };
+
   return (
     <header>
-      <div className="flex items-center px-4">
-        <img src={DKM} alt="logo" className="logo" />
-        <h1 className="text-3xl  md:text-5xl lg:text-6xl  text-nowrap text-neutral-200">The danishKodeMonkey cave</h1>
+      <div className="flex justify-center px-4">
+        <h1 className="font-header text-3xl  md:text-5xl lg:text-6xl  text-nowrap text-neutral-200">The danishKodeMonkey cave</h1>
       </div>
 
       <nav>
@@ -28,29 +32,25 @@ const Header = () => {
         </button>
 
         <div
-          className={`absolute lg:relative right-0 mt-4 lg:ring-2 lg:ring-secondary-dark dark:lg:ring-secondary-light max-lg:rounded-b-lg max-lg:border-secondary-light max-lg:border-b max-lg:border-l max-lg:border-r dark:border-secondary-light dark:bg-secondary-dark bg-secondary max-sm:rounded-b-lg transition-all origin-top duration-300 ease-in-out transform ${
+          className={`absolute lg:relative lg:justify-center right-0 mt-4 lg:ring-2 lg:ring-highlight-light dark:lg:ring-highlight-light max-lg:rounded-b-lg max-lg:border-highlight-light max-lg:border-b max-lg:border-l max-lg:border-r dark:border-highlight-light dark:bg-highlight-dark bg-highlight max-sm:rounded-b-lg transition-all origin-top duration-300 ease-in-out transform ${
             isNavOpen
               ? "block scale-y-100 opacity-100"
-              : "block scale-y-50 opacity-0"
+            : "hidden scale-y-50 opacity-0"
           } lg:flex lg:items-center lg:opacity-100 lg:scale-100`}
         >
           <ul className="flex flex-col lg:flex-row lg:space-x-4 text-right">
-            <li>
-              <Link to={"/"} className="text-5xl lg:text-xl">Home</Link>
-            </li>
-            <li>
-              <Link to={"/contactMe"} className="text-5xl lg:text-xl">Contact</Link>
-            </li>
-            <li>
-              <Link to={"/about"} className="text-5xl lg:text-xl">About</Link>
-            </li>
-
-            <li>
-              <Link to={"/services"} className="text-5xl lg:text-xl">Services</Link>
-            </li>
-            <li>
-              <Link to={"/portfolio"} className="text-5xl lg:text-xl">Portfolio</Link>
-            </li>
+            {["Home", "Contact", "About", "Services", "Portfolio"].map((link) => (
+              <li key={link}>
+                <Link
+                  to={`/${link.toLowerCase()}`}
+                  className={`text-5xl lg:text-xl px-2 transition-all duration-300 ${selectedLink === link ? "px-24 lg:text-3xl lg:px-52 bg-secondary-light dark:bg-secondary-dark hover:text-neutral-dark  ring-2 ring-secondary-light rounded-md" : ""
+                    }`}
+                  onClick={() => handleLinkClick(link)}
+                >
+                  {link}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
