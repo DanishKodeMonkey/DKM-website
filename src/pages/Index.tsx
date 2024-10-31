15cde84d876893f92725058a96adbc2196d8e5a2
danishkodemonkey <humberjini@hotmail.com>
2024-10-31 17:53:27 +0100

    Added more personality to text

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 // @ts-ignore: Deno hates images
-import DKM from "../public/assets/mm12sSNz.jpg";
-
-import { Link } from "react-router-dom";
+import DKM from "../public/assets/WIZARDMONKEY.jpg";
 import { Comment } from "../types.ts";
 import { useLoading } from "../contexts/LoadingContext.tsx";
 import TechScroller from "../components/TechScroller.tsx";
@@ -10,43 +8,34 @@ import TechScroller from "../components/TechScroller.tsx";
 export default function Index() {
   const { setIsLoading } = useLoading();
 
-  const [comments, setComments] = useState<Comment[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      const response = await fetch("/api/comments/");
-
-      const allComments = await response.json() as Comment[];
-      setComments(allComments);
-      setIsLoading(false);
-    })();
-  }, []);
 
   return (
     <>
       <div className="text-center responsive-text">
-        <h1>Hello there!</h1>
-        <div className="flex align-middle justify-center">
+
+        <div className="flex justify-center">
           <img src={DKM} alt="danishKodeMonkey" className="avatar" />
+          <div className="flex-col mt-40">
+            <h1>Hello there! 👋</h1>
+            <h2>
+              My name is <span className="highlight-blue">Daniel</span>
+            </h2>
+            <h3 className="pb-5">
+              I am a{" "}
+              <span className="highlight-amber">Full-stack web wizard!</span>
+            </h3>
+          </div>
         </div>
-        <h3>
-          My name is <span className="highlight-blue">Daniel</span>
-        </h3>
-        <h4>
-          I am a{" "}
-          <span className="highlight-amber">Full-stack web developer!</span>
-        </h4>
+
         <p>
-          I love creating{" "}
-          <span className="font-bold highlight-green">interesting</span> and
-          {" "}
-          <span className="font-bold highlight-pink">intuitive</span>{" "}
-          web solutions, cracking{" "}
-          <span className="font-bold highlight-red">challenges</span> and{" "}
-          <span className=" font-bold highlight-purple">learning</span>{" "}
-          new things!
+          I am on a eternal quest of{" "}
+          <span className="highlight-green">continuous learning</span>.
+          <br />
+          <span className="highlight-pink">Fighting epic battles</span> against <span className="highlight-red">Errors and bugs</span>{" "}
+          <br />
+          While crafting <span className="highlight-yellow">legendary websites</span>, <span className="highlight-green">backend servers </span>and <span className="highlight-red">restful-APIs!</span>
         </p>
+
 
         <TechScroller />
 
