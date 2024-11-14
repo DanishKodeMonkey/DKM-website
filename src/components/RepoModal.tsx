fb55d39771ee12a30fd524e92550e0b1a8bb3b9e
danishkodemonkey <humberjini@hotmail.com>
2024-11-14 13:33:18 +0100

    Animated modals and styling pass of content

--- src/components/RepoModal.tsx
+++ src/components/RepoModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Repo } from "../types.ts";
 
 interface RepoModalProps {
@@ -7,18 +7,46 @@ interface RepoModalProps {
 }
 
 const RepoModal: React.FC<RepoModalProps> = ({ repo, onClose }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    /* set open on mount(appear) to trigger animations*/
+    useEffect(() => {
+        setIsOpen(true)
+    }, [])
+
+    const handleCloseModal = () => {
+        setIsOpen(false);
+        setTimeout(() => {
+            onClose();
+        }, 400)
+    }
     return (
-        <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <button className="modal-close" onClick={onClose}>x</button>
-                <div className="flex justify-between max-sm:flex-col-reverse my-5 max-sm:items-center">
-                    <div className="flex flex-col w-2/3 max-sm:my-5 gap-2 max-sm:w-full">
-                        <h2>{repo.name}</h2>
-                        <p>{repo.description}</p>
-                        <p>{repo.languages}</p>
-                        <p>Created at: {repo.created_at}</p>
-                        <p>Last updated: {repo.updated_at}</p>
-                        <a href={repo.html_url} target="_blank" rel="noopener noreferrer">View on GitHub</a>
+        <div className={`modal-overlay transition-opacity duration-500 ease-in-out ${isOpen ? 'opacity-100' : 'opacity-0'}`} onClick={handleCloseModal}>
+            <div className={`modal-content transition-transform duration-200 ease-in-out transform origin-top ${isOpen ? 'scale-y-100 opacity-100' : 'scale-y-0 opacity-80'}`} onClick={(e) => e.stopPropagation()}>
+                <button className="close-button" onClick={handleCloseModal}>x</button>
+                <div className="flex justify-between max-sm:flex-col-reverse my-5 max-sm:items-center gap-4 h-full">
+                    <div className="flex flex-col w-2/3 max-sm:my-5 max-sm:w-full">
+                        <h3>{repo.name}</h3>
+                        <div className="flex flex-col sm:flex-row border border-highlight-light p-1 text-center space-y-2 sm:space-y-0 sm:space-x-4 mt-2">
+                            <div className="flex-1">
+                                <p className="text-sm">Created at:</p>
+                                <p className="text-sm">{repo.created_at}</p>
+                            </div>
+                            <div className="hidden sm:block border-r border-highlight-light"></div>
+                            <div className="flex-1">
+                                <p className="text-sm">Last updated:</p>
+                                <p className="text-sm">{repo.updated_at}</p>
+                            </div>
+                        </div>
+                        <div className="border border-highlight-light px-2 py-5 h-full">
+                            <p>{repo.description}</p>
+                        </div>
+                        <div className="border border-highlight-light p-2 mb-auto">
+
+                            <p>{repo.languages}</p>
+                        </div>
+
+                        <a href={repo.html_url} target="_blank" rel="noopener noreferrer" >View on GitHub</a>
                     </div>
                     <img src={repo.preview_image} alt="preview image" className="modal-preview" />
                 </div>
